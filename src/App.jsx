7f904829933cf241f6dccbeb37ecc266df4031af
Engi Lozada <engilozada@gmail.com>
2024-02-14 23:29:37 +0100

    Adding some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <div className="text-3xl">App</div>;
+  return (
+    <div className="text-3xl">
+      <h1 className="h1-semibold">Hola</h1>
+      <h2>Hola</h2>
+      <h3>Hola</h3>
+      <h4>Hola</h4>
+      <h6>Hola</h6>
+      <a>Click me</a>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        /*Brand Colors*/
+        "primary-color": "#6962f7",
+        "secondary-color": "#7000ff",
+        "primary-accent": "#0a2540",
+        "primary-button-hover": "#6d7a88",
+        "primary-blue": "#00d4ff",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
